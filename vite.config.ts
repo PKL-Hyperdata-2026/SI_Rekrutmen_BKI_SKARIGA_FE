18ec3e76f31ce15e79bd68334ef9b8ccdc7bb901
kjiovani <kdekgvani@gmail.com>
2026-07-24 11:01:34 +0700

    feat: install redux, shadcn, axios, react-table, leaflet & qr-code

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -23,6 +24,11 @@ export default defineConfig(({ mode }) => {
     },
     preview: {
       port: Number.parseInt(env.VITE_PORT) || 5173,
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
   };
 });
